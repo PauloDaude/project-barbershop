7d3cd01291f2cefd851fb58ac7da349fabb57daf
PauloDaude <paulodaude14@gmail.com>
2024-02-09 19:30:17 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,17 @@
+import { format } from 'date-fns';
 import Header from '../_components/header';
+import { ptBR } from 'date-fns/locale/pt-BR';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <h1>Paulo Daúde</h1>
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' d 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
     </>
   );
 }
