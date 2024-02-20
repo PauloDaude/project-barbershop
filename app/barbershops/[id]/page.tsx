2c98b03536c607f1ffe356d556d8017c2281b423
PauloDaude <paulodaude14@gmail.com>
2024-02-20 12:56:00 -0300

    feat: ensure unauthenticated user is redirected to login on booking click

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -10,6 +10,8 @@ import {
 import Image from 'next/image';
 import BarbershopInfo from './_components/barbershop-info';
 import ServiceItem from './_components/service-items';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -20,6 +22,8 @@ interface BarbershopDetailsPageProps {
 const BarbershopDetailsPage = async ({
   params
 }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
+
   if (!params.id) {
     // TODO: redirecionar para a home page
     return null;
@@ -45,7 +49,11 @@ const BarbershopDetailsPage = async ({
 
       <div className="px-5 flex flex-col gap-4 py-6">
         {barbershop.services.map(service => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem
+            key={service.id}
+            service={service}
+            isAuthenticated={!!session?.user}
+          />
         ))}
       </div>
     </div>
