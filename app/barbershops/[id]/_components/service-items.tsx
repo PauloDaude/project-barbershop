f4daa0cca05295d18b18c3d0bde5df41c2d610a5
PauloDaude <paulodaude14@gmail.com>
2024-02-22 15:30:05 -0300

    feat: add toast on booking success

--- app/barbershops/[id]/_components/service-items.tsx
+++ app/barbershops/[id]/_components/service-items.tsx
@@ -15,11 +15,13 @@ import { Barbershop, Service } from '@prisma/client';
 import { ptBR } from 'date-fns/locale';
 import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { generateDayTimeList } from '../_helpers/houres';
-import { format, setHours, setMinutes } from 'date-fns';
 import { saveBooking } from '../_actions/save-booking';
 import { Loader2 } from 'lucide-react';
+import { toast } from 'sonner';
+import { format, setHours, setMinutes } from 'date-fns';
+import { useRouter } from 'next/navigation';
 
 interface ServiceItemProps {
   barbershop: Barbershop;
@@ -32,11 +34,13 @@ const ServiceItem = ({
   barbershop,
   isAuthenticated
 }: ServiceItemProps) => {
+  const router = useRouter();
   const { data } = useSession();
 
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [hour, setHour] = useState<string | undefined>();
   const [submitIsLoading, setSubmitIsLoading] = useState<boolean>(false);
+  const [sheetIsOpen, setSheetIsOpen] = useState<boolean>(false);
 
   const handleDateClick = (date: Date | undefined) => {
     setDate(date);
@@ -67,6 +71,19 @@ const ServiceItem = ({
         barbershopId: barbershop.id,
         date: newDate,
         userId: (data.user as any).id
+      });
+
+      setSheetIsOpen(false);
+      setHour(undefined);
+      setDate(undefined);
+      toast('Reserva realizada com sucesso!', {
+        description: format(newDate, "'Para' dd 'de' MMMM 'às' HH':'mm'.", {
+          locale: ptBR
+        }),
+        action: {
+          label: 'Visualizar',
+          onClick: () => router.push('/bookings')
+        }
       });
     } catch (error) {
       console.log(error);
@@ -104,7 +121,7 @@ const ServiceItem = ({
                   currency: 'BRL'
                 }).format(Number(service.price))}
               </p>
-              <Sheet>
+              <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
                 <SheetTrigger asChild>
                   <Button variant="secondary" onClick={handleBookingClick}>
                     Reservar
