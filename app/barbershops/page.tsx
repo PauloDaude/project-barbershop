2d3d4204d9bb08b0a65b0ab1f065da761cc0b38a
PauloDaude <paulodaude14@gmail.com>
2024-03-04 09:57:49 -0300

    feat: ensure the search can be memorized

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 import BarbershopItem from '../(home)/_components/barbershop-item';
 import Header from '../_components/header';
 import { db } from '../_lib/prisma';
+import Search from '../(home)/_components/search';
 
 interface BarbershopPageProps {
   searchParams: {
@@ -21,15 +22,20 @@ const BarbershopPage = async ({ searchParams }: BarbershopPageProps) => {
       }
     }
   });
-  console.log(barbershops);
   return (
     <>
       <Header />
-      <div className="px-5 py-6">
+
+      <div className="px-5 py-6 flex flex-col gap-6">
+        <Search
+          defaultValues={{
+            search: searchParams.search
+          }}
+        />
         <h1 className="text-gray-400 font-bold text-xs uppercase">
           Resultados para &quot;{searchParams.search}&quot;
         </h1>
-        <div className="grid grid-cols-2 mt-3 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           {barbershops.map(barbershop => (
             <div className="w-full" key={barbershop.id}>
               <BarbershopItem barbershop={barbershop} />
